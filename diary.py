799bf89ec27a8d87c0ad5567aa35417e5d7233a8
Lester <lesterdeguzman80@gmail.com>
2019-06-27 02:20:16 +0800

    Finished project Added view, delete, search, and clear CLI

--- diary.py
+++ diary.py
@@ -1,5 +1,6 @@
 import datetime
 import sys
+import os
 from collections import OrderedDict
 from peewee import *
 
@@ -23,17 +24,50 @@ def add_entry():
     entry = sys.stdin.read().strip()
 
     if entry:
-        if input("Save entry? Yn").lower != 'n':
+        if input("Save entry? Yn: ").lower != 'n':
             Entry.create(content=entry)
             print("Saved successfully!")
 
 
-def view_entries():
+def view_entries(search_query=None):
     """View Entries"""
-    print("tae tae")
+    entries = Entry.select().order_by(Entry.timestamp.desc())
+
+    if search_query:
+        entries = entries.where(Entry.content.contains(search_query))
+
+    for entry in entries:
+        timestamp = entry.timestamp.strftime("%A %B %d, %Y %I:%M:%p")
+        clear_screen()
+        print(timestamp)
+        print("="*len(timestamp)+"\n")
+        print(entry.content)
+        print("\n"+"="*len(timestamp))
+        print("n) to next entry.")
+        print('d) to delete entry.')
+        print("q) to return to main menu.")
+
+        next_action = input("Action: ").lower().strip()
+        if next_action == 'q':
+            break
+        elif next_action == 'd':
+            delete_entry(entry)
+
+
+def search_entries():
+    """Search entry in your diary."""
+    view_entries(input('Search query: '))
+
 
 def delete_entry(entry):
     """Delete entry."""
+    if input("Are you sure you want to delete this? [Yn]: ").lower() == 'y':
+        entry.delete_instance()
+        print("Entry deleted successfully!")
+
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def initialize():
@@ -44,15 +78,16 @@ def initialize():
 
 menu = OrderedDict([
     ('a', add_entry),
-    ('v', view_entries)
+    ('v', view_entries),
+    ('s', search_entries)
 ])
 
 
 def menu_loop():
     """Show the menu."""
     choice = None
-
     while choice != 'q':
+        clear_screen()
         print("Enter 'q' to quit.")
         for key, value in menu.items():
             print("{}) {}".format(key, value.__doc__))
